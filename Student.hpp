cfb680fefb1bfe941e9d445d11efc35cf8c47a81
Rafał Nowak <nowak9.private@gmail.com>
2018-07-18 11:44:50 +0200

    code formatting

--- Student.hpp
+++ Student.hpp
@@ -8,19 +8,19 @@ class Student
 {
 private:
     int index_;
-	std::string firstName_;
-	std::string lastName_;
+    std::string firstName_;
+    std::string lastName_;
 public:
-	Student();
-	Student(int index, std::string firstName, std::string lastName);
-	~Student();
-	int getIndex() const;
-    	std::string getFirstName() const;
-    	std::string getLastName() const;
-	void showStudent() const;
-    	void setIndex(int);
-    	void setFirstName(std::string);
-    	void setLastName(std::string);
-	friend std::ostream& operator<<(std::ostream& os, const Student& s);
-	friend std::istream& operator>>(std::istream& is, Student& s);
+    Student();
+    Student(int index, std::string firstName, std::string lastName);
+    ~Student();
+    int getIndex() const;
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    void showStudent() const;
+    void setIndex(int);
+    void setFirstName(std::string);
+    void setLastName(std::string);
+    friend std::ostream& operator<<(std::ostream& os, const Student& s);
+    friend std::istream& operator >> (std::istream& is, Student& s);
 };
