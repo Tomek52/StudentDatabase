ab2804f7b02bf9f67d5dee8d4d5b7108581faa75
Rafał Nowak <nowak9.private@gmail.com>
2018-07-21 21:19:24 +0200

    Improved getIndex function

--- Student.cpp
+++ Student.cpp
@@ -1,4 +1,5 @@
-#include "Student.hpp"
+#include "Student.h"
+#include <limits>
 
 Student::Student(int index,
     std::string firstName,
@@ -27,7 +28,48 @@ std::string Student::getLastName() const
 
 void Student::setIndex(int index)
 {
-    index_ = index;
+    bool correctIndex = false;
+    bool clear = false;
+    double checkIndex = index / 100000;
+    if (checkIndex >= 1.0 && checkIndex < 9.9)
+        index_ = index;
+    else
+    {
+        std::cout << "Incorrect index. Your index must contains 6 numbers !" << std::endl;
+        do
+        {
+            std::cout << "Input correct index: ";
+            if (clear)
+            {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            std::cin >> index_;
+
+            if (!std::cin)
+            {
+                std::cout << std::endl;
+                std::cout << "Index can contains only numbers !" << std::endl;
+                correctIndex = false;
+                clear = true;
+            }
+            else
+            {
+                checkIndex = index_ / 100000;
+                if (checkIndex < 1.0 || checkIndex > 9.9)
+                {
+                    std::cout << "Incorrect index. Your index must contains 6 numbers !" << std::endl;
+                    std::cout << "First number cannot be 0 !" << std::endl;
+                    correctIndex = false;
+                    clear = true;
+                }
+                else
+                {
+                    correctIndex = true;
+                }
+            }
+        } while (correctIndex == false);
+    }
 }
 
 void Student::setFirstName(std::string firstName)
