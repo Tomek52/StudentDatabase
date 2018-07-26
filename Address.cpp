d19af44fcf9742eeee4952574ae631dd95f77ada
t0m3k52 <tomek.szpytma@gmail.com>
2018-07-26 12:27:42 +0200

    added implementation of constructor and destructor for address

--- Address.cpp
+++ Address.cpp
@@ -1,1 +1,19 @@
 #include "Address.hpp"
+
+
+Address::Address(std::string city,std::string zipCode, std::string::street, int streetNumber, int apartmentNumber)
+    : city_(city)
+    , zipCode_(zipCode)
+    , street_(street)
+    , streetNumber_(streetNumber)
+    , apartmentNumber_(apartmentNumber)
+{}
+
+Address::Address(std::string city,std::string zipCode, std::string::street, int streetNumber);
+    : city_(city)
+    , zipCode_(zipCode)
+    , street_(street)
+    , streetNumber_(streetNumber)
+{}
+
+Address::~Address() {}
