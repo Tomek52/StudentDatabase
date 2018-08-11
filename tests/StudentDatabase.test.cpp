b3fe370b0478315bfaf983687eb9a7738b132323
t0m3k52 <tomek.szpytma@gmail.com>
2018-08-11 17:42:14 +0200

    added unit test

---
+++ tests/StudentDatabase.test.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+
+SCENARIO("test")
+{
+    GIVEN("asd")
+    {
+        WHEN("aaa")
+        {
+            THEN("www")
+            {
+                REQUIRE(1==1);
+            }
+        }
+    }
+}
