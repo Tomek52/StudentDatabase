ce4673ad3920bfab73810fa11d076c0dbb54000a
[szymon] <[szymon.janas.95@gmail.com]>
2018-10-02 13:05:39 +0200

    add test add_new_employee_by_constructor

--- tests/personTest.cpp
+++ tests/personTest.cpp
@@ -11,6 +11,12 @@ struct testPerson : public ::testing::Test
                                "F",
                                "ul. Głogowska 142, 60-205 Poznań ",
                                566854);
+    Person* em1 = new Employee("52102885074",
+                              "Karl",
+                              "A. Lucero",
+                              "M",
+                              "ul. Gościnna 62, 15-157 Białystok",
+                              8652);
 };
 
 TEST_F(testPerson, add_new_student_by_constructor)
@@ -23,3 +29,12 @@ TEST_F(testPerson, add_new_student_by_constructor)
     ASSERT_EQ(566854, st1->getIndex());
 }
 
+TEST_F(testPerson, add_new_employee_by_constructor)
+{
+    ASSERT_EQ("52102885074", em1->getPesel());
+    ASSERT_EQ("Karl", em1->getFirstName());
+    ASSERT_EQ("A. Lucero", em1->getLastName());
+    ASSERT_EQ("M", em1->getSex());
+    ASSERT_EQ("ul. Gościnna 62, 15-157 Białystok", em1->getAddress());
+    ASSERT_EQ(8652, em1->getSalary());
+}
